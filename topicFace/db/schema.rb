4abbcb05d8684c1850d6095cba53c94d4e5d66d3
DANIEL RESTREPO <Daniel@DANIELs-MacBook-Pro.local>
2017-08-07 16:21:56 -0500

    Migrando a postgresql

--- topicFace/db/schema.rb
+++ topicFace/db/schema.rb
@@ -12,13 +12,8 @@
 
 ActiveRecord::Schema.define(version: 20170806011144) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "tittle"
-    t.string "e"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "images", force: :cascade do |t|
     t.string "tittle"
